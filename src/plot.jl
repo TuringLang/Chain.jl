12af4078af9f7f11f259c5b73867264d5871186d
Paulina Martin <paulinamartin96@ciencias.unam.mx>
2021-07-18 00:14:18 -0500

    Add prior/posterior predictive check plot

--- src/plot.jl
+++ src/plot.jl
@@ -4,12 +4,14 @@
 @shorthands pooleddensity
 @shorthands traceplot
 @shorthands corner
+@shorthands ppcplot
 
 struct _TracePlot; c; val; end
 struct _MeanPlot; c; val;  end
 struct _DensityPlot; c; val;  end
 struct _HistogramPlot; c; val;  end
 struct _AutocorPlot; lags; val;  end
+struct _PPCPlot; y_obs; y_pred; ymean_pred; end
 
 # define alias functions for old syntax
 const translationdict = Dict(
@@ -18,7 +20,8 @@ const translationdict = Dict(
                         :density => _DensityPlot,
                         :histogram => _HistogramPlot,
                         :autocorplot => _AutocorPlot,
-                        :pooleddensity => _DensityPlot
+                        :pooleddensity => _DensityPlot,
+                        :ppcplot => _PPCPlot
                       )
 
 const supportedplots = push!(collect(keys(translationdict)), :mixeddensity, :corner)
@@ -183,4 +186,104 @@ end
     size --> (600, 600)
     ar = collect(Array(corner.c.value[:, corner.parameters,i]) for i in chains(corner.c))
     RecipesBase.recipetype(:cornerplot, vcat(ar...))
+end
+
+@recipe function f(
+    yobs_data,
+    ypred_data::Chains,
+    check_type = :posterior,
+    n_samples::Int = 50;
+    yvar_name::AbstractVector{<:String}
+    )
+
+    st = get(plotattributes, :seriestype, :traceplot)
+
+    if st == :ppcplot
+        N = n_samples <= size(ypred_data)[1] ? n_samples : size(ypred_data)[1]
+        index = sample(1:size(ypred_data)[1], N, replace = false, ordered = true)
+        if check_type == :posterior
+            title --> "Posterior predictive check"
+        elseif check_type == :prior
+            title --> "Prior predictive check"
+        else
+            throw(ArgumentError("`check_type` must be one of `prior` or `posterior`"))
+        end
+
+        if ndims(yobs_data) == 1
+            #y_obs = yobs_data
+            n_yvar = 1
+            n_yval = size(yobs_data)[1] #y values per sample
+            predictions = ypred_data.value.data[index,:,:]
+            ymean_pred = vec(mean(ypred_data.value.data, dims = 1))
+            @series begin
+                seriestype := :density
+                label --> "Y obs"
+                y_obs = yobs_data
+            end
+            @series begin
+                seriestype := :density
+                label --> "Y mean"
+                ymean_pred
+            end
+            for i in 1:N
+                @series begin
+                    seriestype := :density
+                    seriesalpha --> 0.2
+                    linecolor --> "#BBBBBB"
+                    label --> nothing
+                    y_pred = predictions[i,:,:]
+                end
+            end
+        elseif ndims(yobs_data) >= 1 ## ordered by columns
+            n_yval = size(yobs_data)[1]
+            n_yvar = size(yobs_data)[2] ## number of dependent variables
+            #n_iter = size(ypred_data)[1]
+            y_obs = yobs_data ## observed data for y
+            ymean_pred = reshape(mean(ypred_data.value.data, dims = 1), (n_yvar, n_yval))
+            #predictions = reshape(ypred_data.value.data, (n_iter,n_yval,n_yvar))[index,:,:]
+            #sections = [MCMCChains.group(ypred_data, Symbol(yvar_name[i])).value.data[index,:,:]
+            #            for i in 1:n_yvar]
+            layout := (1, n_yvar)
+            k = 0
+            for j in 1:n_yvar
+                sections = MCMCChains.group(ypred_data, Symbol(yvar_name[j]))
+                predictions = sections.value.data[index,:,:]
+                #sections = dropdims(group(ypred_data, yvar_name[j]).value.data, dims = 3)
+                #predictions = reshape(ypred_data.value.data, (n_iter,n_yval,n_yvar))[index,:,:]
+                k += 1
+                @series begin
+                    subplot := k
+                    seriestype := :density
+                    label --> "Yobs"
+                    y_obs[:,j]
+                end
+                @series begin
+                    subplot := k
+                    seriestype := :density
+                    label --> "Ymean"
+                    ymean_pred[j,:]
+                end
+                for i in 1:N
+                    #y_pred = sections[j][i,:,:]
+                    @series begin
+                        subplot := k
+                        seriestype := :density
+                        seriesalpha --> 0.2
+                        linecolor --> "#BBBBBB"
+                        label --> nothing
+                        y_pred = predictions[i,:,:]
+                    end
+                end
+            end
+        else
+            throw(ArgumentError("Observed data must have `dim > 1`"))
+        end
+    else
+
+    end
+end
+
+@recipe function f(p::_PPCPlot)
+    #title --> "Posterior predictive check"
+    p.y_obs, p.y_pred
 end
